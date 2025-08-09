763dc1a2d69a60b84cbe0e2eec75a38c8fd46be7
anniemx <163184360+anniemx@users.noreply.github.com>
2025-08-09 21:23:24 +0300

    Use CSS and improve reviews list

--- reviews.py
+++ reviews.py
@@ -25,7 +25,10 @@ def get_classes(review_id):
     return db.query(sql, [review_id])
 
 def get_reviews():
-    sql = "SELECT id, title FROM reviews ORDER BY id DESC"
+    sql = """SELECT reviews.id, reviews.title, users.id user_id, users.username
+             FROM reviews, users
+             WHERE reviews.user_id = users.id
+             ORDER BY reviews.id DESC"""
     return db.query(sql)
 
 def get_review(review_id):
