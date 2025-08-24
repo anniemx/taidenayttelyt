09732cb8aaf8c1b4effa3a27a7e27c082f554f40
anniemx <163184360+anniemx@users.noreply.github.com>
2025-08-24 13:32:46 +0300

    Add testing for large data quantities

---
+++ seed.py
@@ -0,0 +1,33 @@
+import random
+import sqlite3
+
+db = sqlite3.connect("database.db")
+
+db.execute("DELETE FROM users")
+db.execute("DELETE FROM exhibitions")
+db.execute("DELETE FROM comments")
+
+user_count = 1000
+exhibition_count = 10**5
+comment_count = 10**6
+
+for i in range(1, user_count + 1):
+    db.execute("INSERT INTO users (username) VALUES (?)",
+               ["user" + str(i)], )
+
+for i in range(1, exhibition_count + 1):
+    user_id = random.randint(1, user_count)
+    db.execute("INSERT INTO exhibitions (title, place, time, location, description, user_id) VALUES (?, ?, ?, ?, ?, ?)",
+               ["exhibition" + str(i), "place"+ str(i), "25.05.2025", "location"+ str(i), "description"+ str(i), user_id])
+
+
+for i in range(1, comment_count + 1):
+    user_id = random.randint(1, user_count)
+    exhibition_id = random.randint(1, exhibition_count)
+    evaluation = random.randint(1, 5)
+    db.execute("""INSERT INTO comments (title, content, sent_at, user_id, evaluation, exhibition_id)
+                  VALUES (?, ?, datetime("now"), ?, ?, ?)""",
+               ["title"+ str(i), "comment" + str(i), user_id, evaluation, exhibition_id])
+
+db.commit()
+db.close()
